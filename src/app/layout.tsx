b72155c4e508e78b0967aea5d448868cae707471
Shenhaoyi <shenhaoyi04@163.com>
2024-10-17 03:20:29 +0800

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AntdRegistry>{children}</AntdRegistry>
       </body>
+      <GoogleTagManager gtmId="G-9G5BFT0VHL" />
     </html>
   );
 }
